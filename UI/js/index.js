c3b28e140680cbe1794c5e0817e394a76dde12d3
anjolabassey <anjolabassey@yahoo.com>
2018-07-13 00:09:17 +0100

    #158908091 build sin up modal

--- UI/js/index.js
+++ UI/js/index.js
@@ -2,7 +2,9 @@ let loginModal = document.getElementById("loginmodal");
 let signupModal = document.getElementById("signupmodal");
 let viewLogin = document.getElementById("view-login");
 let viewSignup = document.getElementById("view-signup");
-let close = document.getElementById("close");
+let viewSignup2 = document.getElementById("view-signup2");
+let closeLogin = document.getElementById("close1");
+let closeSignup = document.getElementById("close2");
 let submitLogin = document.getElementById("submitlogin");
 let submitSignup = document.getElementById("submitsignup");
 
@@ -17,16 +19,27 @@ viewSignup.addEventListener("click", event => {
     signupModal.style.display = "block";
 
 });
+viewSignup2.addEventListener("click", event => {
+    event.preventDefault();
+    signupModal.style.display = "block";
 
+});
 window.onclick = event => {
-    if (event.target == modal) {
-        modal.style.display = "none";
+    if (event.target === loginModal) {
+        loginModal.style.display = "none";
+    }
+    if (event.target === signupModal) {
+        signupModal.style.display = "none";
     }
 }
 
-close.addEventListener("click", event => {
-    modal.style.display = "none";
+closeLogin.addEventListener("click", event => {
+    loginModal.style.display = "none";
+    
 
+});
+closeSignup.addEventListener("click", event => {
+    signupModal.style.display = "none";
 });
 
 submitLogin.addEventListener("click", event => {
