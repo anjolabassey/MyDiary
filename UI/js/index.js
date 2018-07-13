758d5dc1588803e34b95b29bcc29cee9b348da71
anjolabassey <anjolabassey@yahoo.com>
2018-07-13 08:04:17 +0100

    comment js code

--- UI/js/index.js
+++ UI/js/index.js
@@ -8,12 +8,14 @@ let closeSignup = document.getElementById("close2");
 let submitLogin = document.getElementById("submitlogin");
 let submitSignup = document.getElementById("submitsignup");
 
+//show log in when login button is clicked
 viewLogin.addEventListener("click", event => {
     event.preventDefault();
     loginModal.style.display = "block";
 
 });
 
+//show Sign up when sign up button is clicked
 viewSignup.addEventListener("click", event => {
     event.preventDefault();
     signupModal.style.display = "block";
@@ -24,6 +26,8 @@ viewSignup2.addEventListener("click", event => {
     signupModal.style.display = "block";
 
 });
+
+//close log in/sign up modal when user clicks outside of it 
 window.onclick = event => {
     if (event.target === loginModal) {
         loginModal.style.display = "none";
@@ -33,21 +37,26 @@ window.onclick = event => {
     }
 }
 
+//close log in modal when user clicks close button
 closeLogin.addEventListener("click", event => {
     loginModal.style.display = "none";
     
 
 });
+
+//close sign up modal when user clicks close button 
 closeSignup.addEventListener("click", event => {
     signupModal.style.display = "none";
 });
 
+//directs user to dashboard after clicking submit
 submitLogin.addEventListener("click", event => {
     event.preventDefault();
     location.assign("dashboard.html")
 
 });
 
+//directs user to dashboard after clicking submit
 submitSignup.addEventListener("click", event => {
     event.preventDefault();
     location.assign("dashboard.html")
--- UI/js/main.js
+++ UI/js/main.js
@@ -1,6 +1,7 @@
 let del = document.getElementById("delete");
 let popUp = document.getElementById("popup");
 
+//toggle delete pop up when delete icon is clicked
 del.addEventListener("click", (event) => {
     popUp.classList.toggle("show");
 
