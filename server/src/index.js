6495ee0f24090d7b6796df32f232d1473d64fcdd
anjolabassey <anjolabassey@yahoo.com>
2018-07-17 21:27:09 +0100

    setup server

--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,15 @@
+
 import express from 'express';
+
+const port = process.env.port || 4000;
+
+// setup express app
+const app = express();
+
+// listen for requests
+app.listen(port, () => {
+  console.log(`Started on port ${port}`);
+});
+
+
+export default app;
