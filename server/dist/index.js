699395c2258d1f51b8796907d37d3e6914e509b0
anjolabassey <anjolabassey@yahoo.com>
2018-07-18 11:45:19 +0100

    chore: create test file

--- server/dist/index.js
+++ server/dist/index.js
@@ -1,8 +1,31 @@
 'use strict';
 
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
 var _express = require('express');
 
 var _express2 = _interopRequireDefault(_express);
 
+var _bodyParser = require('body-parser');
+
+var _bodyParser2 = _interopRequireDefault(_bodyParser);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var port = process.env.port || 4000;
+
+// setup express app
+var app = (0, _express2.default)();
+
+app.use(_bodyParser2.default.json());
+app.use(_bodyParser2.default.urlencoded({ extended: false }));
+
+// listen for requests
+app.listen(port, function () {
+  console.log('Started on port ' + port);
+});
+
+exports.default = app;
 //# sourceMappingURL=index.js.map
