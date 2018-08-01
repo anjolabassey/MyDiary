9c83042547b05b65abb943cf2fcf0e17ffef3ee5
anjolabassey <anjolabassey@yahoo.com>
2018-08-01 13:49:40 +0100

    feat(user-login API): add user login functionality - create login controller with functinality - create login route
    [Delivers #159427655]

--- server/src/controllers/users.js
+++ server/src/controllers/users.js
@@ -1,8 +1,8 @@
 // solution for validating email was gotten from https://www.codeproject.com/Tips/492632/Email-Validation-in-JavaScript
 
+import bcrypt from 'bcrypt';
 import { db, client } from '../models/database';
 
-const bcrypt = require('bcrypt');
 /**
  * @export
  * @class Entrycontroller
@@ -16,7 +16,7 @@ export default class Usercontroller {
    */
   static signup(req, res) {
     const { email, username, password } = req.body;
-    const hash = bcrypt.hash(password, 10);
+    const hashed = bcrypt.hashSync(password, 10);
     const re = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
 
     if (!email || !username || !password) {
@@ -30,14 +30,11 @@ export default class Usercontroller {
     } else if (re.test(email) === false) {
       return res.status(400).json({ message: 'Please enter a valid email' });
     } else {
-      client.query('INSERT INTO users (email, username, password) VALUES ( $1, $2, $3) RETURNING *', [email, username, password], (err, resp) => {
+      client.query('INSERT INTO users (email, username, password) VALUES ( $1, $2, $3) RETURNING *', [email, username, hashed], (err, resp) => {
         if (err) {
-          return res.status(409).json({ error: 'An account with this email already exists' });
+          return res.status(409).json({ error: err, pwd: hashed });
         } else {
-          return res.json({
-            message: 'User successfully added',
-            entry: resp.row
-          });
+          return res.json({ message: 'User successfully added' });
         }
       });
     }
@@ -58,11 +55,14 @@ export default class Usercontroller {
       client.query(`SELECT * FROM users WHERE email ='${email}'`, (err, resp) => {
         if (err) {
           return res.status(400).json({ error: 'Wrong email or password, please try again' });
+        } else if (resp.rowCount === 0) {
+          return res.status(400).json({ message: 'This email address does not exist' });
         } else {
-          return res.json({
-            message: 'User here',
-            entry: resp.rows
-          });
+          if (bcrypt.compareSync(password, resp.rows[0].password)) {
+            return res.json({ message: `${resp.rows[0].email} is signed in` });
+          } else {
+            return res.status(400).json({ messsage: 'Incorrect password' });
+          }
         }
       });
     }
